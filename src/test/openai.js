aac10c4befdbca9cf811aaf37c1f2b1b64826771
liushuai166 <liushuai166@ke.com>
2023-03-27 17:08:47 +0800

    feat:robot

--- src/test/openai.js
+++ src/test/openai.js
@@ -5,11 +5,24 @@ const configuration = new Configuration(openAiConfig);
 
 const openai = new OpenAIApi(configuration);
 
-openai.createChatCompletion({
+const imageConfig = {
+    organization: "org-YBnhLiKGZYTvq4FdJqjVtUht",
+    apiKey: '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',
+}
+const imageConfiguration = new Configuration(imageConfig);
+const iamgeOpenai = new OpenAIApi(imageConfiguration);
+
+iamgeOpenai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [{ "role": "user", "content": "写一个动滑轮 定滑轮的教案" }],
+    messages: [{ "role": "user", "content": "你好" }],
 }).then(res => {
     console.log("res------->", res.data.choices.map(c => {
         console.log("message----->\n", c.message)
     }))
 })
+
+iamgeOpenai.createImage({
+    prompt: "一只猫咪",
+    n: 1,
+    size: "1024x1024"
+}).then(res => console.log("res------>", res.data))
